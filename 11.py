21c05cb7e932930ebc7ff47d11a404f386acca7b
Shimon Lyons <shimonl@tzurmanagement.com>
2022-12-12 14:14:45 +0200

    11b finally

--- 11.py
+++ 11.py
@@ -2,39 +2,41 @@ from typing import Callable
 from aocd import data, submit
 
 
-data = """Monkey 0:
-  Starting items: 79, 98
-  Operation: new = old * 19
-  Test: divisible by 23
-    If true: throw to monkey 2
-    If false: throw to monkey 3
+# data = """Monkey 0:
+#   Starting items: 79, 98
+#   Operation: new = old * 19
+#   Test: divisible by 23
+#     If true: throw to monkey 2
+#     If false: throw to monkey 3
+#
+# Monkey 1:
+#   Starting items: 54, 65, 75, 74
+#   Operation: new = old + 6
+#   Test: divisible by 19
+#     If true: throw to monkey 2
+#     If false: throw to monkey 0
+#
+# Monkey 2:
+#   Starting items: 79, 60, 97
+#   Operation: new = old * old
+#   Test: divisible by 13
+#     If true: throw to monkey 1
+#     If false: throw to monkey 3
+#
+# Monkey 3:
+#   Starting items: 74
+#   Operation: new = old + 3
+#   Test: divisible by 17
+#     If true: throw to monkey 0
+#     If false: throw to monkey 1"""
 
-Monkey 1:
-  Starting items: 54, 65, 75, 74
-  Operation: new = old + 6
-  Test: divisible by 19
-    If true: throw to monkey 2
-    If false: throw to monkey 0
-
-Monkey 2:
-  Starting items: 79, 60, 97
-  Operation: new = old * old
-  Test: divisible by 13
-    If true: throw to monkey 1
-    If false: throw to monkey 3
-
-Monkey 3:
-  Starting items: 74
-  Operation: new = old + 3
-  Test: divisible by 17
-    If true: throw to monkey 0
-    If false: throw to monkey 1"""
+divisors = (2,3,5,7,11,13,17,19)
 
 class Monkey:
     def __init__(self, monkeys: list, monkeydata: str):
         self.monkeys = monkeys
         monkeydata = monkeydata.splitlines()[1:]
-        self.items = [int(x) for x in monkeydata.pop(0).split(': ')[1].split(', ')]
+        self.items = [{z: int(x) % z for z in divisors} for x in monkeydata.pop(0).split(': ')[1].split(', ')]
         op_parts = monkeydata.pop(0).split(' ')[-2:]
         if op_parts[0] == '*':
             if op_parts[1] == 'old':
@@ -46,7 +48,7 @@ class Monkey:
         test = int(monkeydata.pop(0).split(' ')[-1])
         if_true = int(monkeydata.pop(0).split(' ')[-1])
         if_false = int(monkeydata.pop(0).split(' ')[-1])
-        self.test = lambda x: if_true if x % test == 0 else if_false
+        self.test = lambda x: if_true if x[test] == 0 else if_false
         self.inspections = 0
 
     def receive(self, item: int):
@@ -61,9 +63,7 @@ class Monkey:
             self.do_item(item)
 
     def do_item(self, item):
-        item = self.op(item)
-        item = item % 5681 # 9699690
-        item = int(item/3)
+        item = {n: self.op(x) % n for (n, x) in item.items()}
         self.inspections += 1
         self.throw(item, self.test(item))
 
@@ -71,7 +71,7 @@ class Monkey:
 monkeys = []
 monkeys.extend([Monkey(monkeys, x) for x in data.split('\n\n')])
 
-for i in range(20 ):
+for i in range(10000):
     # print("Round", i)
     for (j, monkey) in enumerate(monkeys):
         monkey.turn()
@@ -79,35 +79,4 @@ for i in range(20 ):
     # print("\n")
 print([x.inspections for x in monkeys])
 inspections = sorted((x.inspections for x in monkeys), reverse=True)[0:2]
-print(inspections[0]*inspections[1])
-
-
-class Item:
-    def __init__(self, init):
-        self.factors = {}
-        total = 1
-        for n in (2,3,5,7,11,13,17,19):
-            self.factors[n] = 0
-            while init % n == 0:
-                self.factors[n] += 1
-                total *= self.factors[n]
-                init /= n
-        self.remainder = init - total
-
-    def multiply(self, by):
-        self.factors[by] += 1
-        self.remainder *= by
-        self.remainder %= 9699690
-
-    def add(self, amt):
-        self.remainder += amt
-        self.remainder %= 9699690
-
-    def square(self):
-        for k in self.factors:
-            self.factors[k] *= 2
-
-    def is_divisible(self, by):
-        return self.factors[by] and self.remainder % by == 0
-
-# Go through each item
+submit(inspections[0]*inspections[1])
