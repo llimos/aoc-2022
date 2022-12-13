7d7d06c47ba01c96c917906851286625b9e63869
Shimon Lyons <shimonl@tzurmanagement.com>
2022-12-14 00:48:15 +0200

    cleanup

--- 13.py
+++ 13.py
@@ -4,25 +4,19 @@ from functools import cmp_to_key
 
 
 def inorder(left, right) -> int:
+    if isinstance(left, int) and isinstance(right, int):
+        return left - right
     if isinstance(left, int):
-        if isinstance(right, int):
-            return left - right
         return inorder([left], right)
     if isinstance(right, int):
         return inorder(left, [right])
-    if len(left) == 0:
-        return 0 - len(right)
-    if len(right) == 0:
-        return 1
+    if len(left) == 0 or len(right) == 0:
+        return len(left) - len(right)
     return inorder(left[0], right[0]) or inorder(left[1:], right[1:])
 
-
-print(sum([i + 1
-           for (i, pair) in enumerate(data.split('\n\n'))
+print(sum([i + 1 for (i, pair) in enumerate(data.split('\n\n'))
            if inorder(loads(pair.split('\n')[0]), loads(pair.split('\n')[1])) < 0]))
 
 # part 2
-packets = [loads(packet) for packet in data.splitlines() if packet != '']
-packets.extend(([[2]], [[6]]))
-packets = sorted(packets, key=cmp_to_key(inorder))
+packets = sorted([loads(packet) for packet in data.split('\n') if packet != '']+[[[2]], [[6]]], key=cmp_to_key(inorder))
 submit((packets.index([[2]]) + 1) * (packets.index([[6]]) + 1))
