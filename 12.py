3744071f8dd46bb0bd6860e0a6625028e7c9aff5
Shimon Lyons <shimonl@tzurmanagement.com>
2022-12-13 13:59:29 +0200

    Cleanup

--- 12.py
+++ 12.py
@@ -10,8 +10,7 @@ candidates = {(y, x) for (y, line) in enumerate(lines) for (x, l) in enumerate(l
 seen = set(candidates)
 i = 0
 while end not in candidates:
-    candidates = {(ty, tx)
-                  for (y, x) in candidates for (ty, tx) in ((y-1, x), (y+1, x), (y, x-1), (y, x+1))
+    candidates = {(ty, tx) for (y, x) in candidates for (ty, tx) in ((y-1, x), (y+1, x), (y, x-1), (y, x+1))
                   if 0 <= ty < len(lines) and 0 <= tx < len(lines[ty])
                   and (ty, tx) not in seen and level(lines[ty][tx]) <= level(lines[y][x]) + 1}
     seen.update(candidates)
