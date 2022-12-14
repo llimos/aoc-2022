847af5df83bc47a68cfbdb647d8158593f05151a
Shimon Lyons <shimonl@tzurmanagement.com>
2022-12-15 00:15:24 +0200

    cleaner

--- 14.py
+++ 14.py
@@ -5,18 +5,18 @@ blocked = set()
 floor = 0
 for line in lines:
     prev = line[0]
-    for coord in line:
+    for curr in line:
         blocked.update({(x, y)
-                        for x in range(min(prev[0], coord[0]), max(prev[0], coord[0]) + 1)
-                        for y in range(min(prev[1], coord[1]), max(prev[1], coord[1]) + 1)})
-        floor = max(floor, coord[1] + 2)
-        prev = coord
+                        for x in range(min(prev[0], curr[0]), max(prev[0], curr[0]) + 1)
+                        for y in range(min(prev[1], curr[1]), max(prev[1], curr[1]) + 1)})
+        floor = max(floor, curr[1] + 2)
+        prev = curr
 
 n = 0
 while (500, 0) not in blocked:
     n += 1
     (x, y) = (500, 0)
-    while y + 1 < floor and not blocked.issuperset({(x, y+1), (x-1, y+1), (x+1, y+1)}):
+    while y < floor - 1 and not blocked.issuperset({(x, y+1), (x-1, y+1), (x+1, y+1)}):
         (x, y) = next(filter(lambda z: z not in blocked, ((x, y+1), (x-1, y+1), (x+1, y+1))))
     blocked.add((x, y))
 submit(n)
