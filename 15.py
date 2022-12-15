96fe89d41a159b4e78a363181fd736287f96d63c
Shimon Lyons <shimonl@tzurmanagement.com>
2022-12-15 21:30:45 +0200

    15b. Slower than 15s but it works so I don't care

--- 15.py
+++ 15.py
@@ -16,21 +16,52 @@ from aocd import lines, submit
 # Sensor at x=20, y=1: closest beacon is at x=15, y=3""".splitlines()
 
 lines = [[[int(part.split('=')[1]) for part in parts.split(', ')] for parts in line.split(':')] for line in lines]
-
-lines_with_distance = [[[x, y], abs(bx-x)+abs(by-y)] for ((x, y), (bx, by)) in lines]
-
+lines = [((x, y), (bx, by), abs(bx - x) + abs(by - y)) for ((x, y), (bx, by)) in lines]
+limit = 4000000
 
 def no_beacon(row):
-    exclude = set()
-    beacons = set()
-    for ((x, y), (bx, by)) in lines:
-        d = abs(bx - x) + abs(by - y)
-        if by == row:
-            beacons.add(bx)
+    exclude: set[tuple[int, int]] = set()
+    for ((x, y), _, d) in lines:
         if abs(y-row) <= d:
             spare = d - abs(y-row)
-            exclude.update(range(x-spare, x+spare+1))
-    return len(exclude.difference(beacons))
+            excl_from = max(0, x-spare)
+            excl_to = min(limit, x+spare)
+            overlapping = [(x1, x2) for (x1, x2) in exclude if not (x2 < excl_from - 1 or x1 > excl_to + 1)]
+            merged = (min([x1 for (x1, x2) in overlapping] + [excl_from]), max([x2 for (x1, x2) in overlapping] + [excl_to]))
+            exclude = exclude.difference(overlapping) | {merged}
+    if len(exclude) == 2:
+        [first, second] = list(exclude)
+        return first[1] + 1 if first[0] < second[0] else second[1] + 1
+    else:
+        [start, end] = list(exclude)[0]
+        if start > 0:
+            return 0
+        if end < limit:
+            return limit
 
 
-submit(no_beacon(2000000))
+
+
+# submit(no_beacon(2000000))
+
+# Part 2
+
+
+# exclude = set()
+# for ((x, y), (bx, by)) in lines:
+#     d = abs(bx - x) + abs(by - y)
+#     for tx in range(max(0, x-d), min(limit, x+d)+1):
+#         spare = d - abs(tx - x)
+#         for ty in range(max(0, y - spare), min(limit, y + spare) + 1):
+#             exclude.add((tx, ty))
+
+# submit(next(x * 4000000 + y for x in range(0, limit+1) for y in range(0, limit+1) if (x, y) not in exclude))
+
+for row in range(limit + 1):
+    if row % 5000 == 0:
+        print(row)
+    x = no_beacon(row)
+    if x is not None:
+        print((x, row))
+        break
+
